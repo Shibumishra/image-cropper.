c92f071692de32526415eec5d06876edbc1bd006
Your Name <you@example.com>
2021-09-20 00:11:11 +0530

    Updates

---
+++ precache-manifest.5cacddd7536e999f56fda801fb0169c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e9c6a93579dcfd82f452d3cf9471b5",
+    "url": "/image-cropper./index.html"
+  },
+  {
+    "revision": "bf55f9c725b7238fd9e8",
+    "url": "/image-cropper./static/css/2.646a9642.chunk.css"
+  },
+  {
+    "revision": "62a6642b081204322e4d",
+    "url": "/image-cropper./static/css/main.031cfd0b.chunk.css"
+  },
+  {
+    "revision": "bf55f9c725b7238fd9e8",
+    "url": "/image-cropper./static/js/2.f3a59ef9.chunk.js"
+  },
+  {
+    "revision": "bc7f16ba0faefd95429f81c872826b69",
+    "url": "/image-cropper./static/js/2.f3a59ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a6642b081204322e4d",
+    "url": "/image-cropper./static/js/main.c0cca675.chunk.js"
+  },
+  {
+    "revision": "83a458b05f32fd6aed34",
+    "url": "/image-cropper./static/js/runtime-main.2a684647.js"
+  }
+]);
